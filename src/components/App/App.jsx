10cd2c5524840d4dce572233120b91713a527c81
Sean Wade <smwade1115@gmail.com>
2021-01-30 18:22:47 -0600

    styling to header, routing of home button fixed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,16 @@
 import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import Header from '../Header/Header'
+import Header from '../Header/Header';
 import MovieList from '../MovieList/MovieList';
 import Details from '../Details/Details';
-import AddMovie from '../AddMovie/AddMovie'
-import EditMovie from '../EditMovie/EditMovie'
+import AddMovie from '../AddMovie/AddMovie';
+import EditMovie from '../EditMovie/EditMovie';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Router>
-        <Header />
         {/* home page is movie list */}
         <Route path="/" exact component={MovieList} />
         {/* details and edit pages route to dedicated ids */}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,15 @@
-import { useHistory } from 'react-router-dom';
+import { HashRouter as Router, Link } from 'react-router-dom';
+import './Header.css';
 
 function Header() {
-    const history = useHistory();
-
   return (
-    <header>
-      <h1>The Movies Saga!</h1>
-      {/* handy little home button for every page */}
-      <button onClick={() => {history.push('/')}}>Home</button>
-    </header>
+    <Router>
+      <header>
+        <h1>The Movies Saga!</h1>
+        {/* handy little home button for every page */}
+        <Link to="/"><button>Home</button></Link>
+      </header>
+    </Router>
   );
 }
 
