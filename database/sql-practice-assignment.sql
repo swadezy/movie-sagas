ba571814309657038ec830d1b22a205a49423675
Sean Wade <smwade1115@gmail.com>
2021-02-01 22:18:28 -0600

    sql completed

--- database/sql-practice-assignment.sql
+++ database/sql-practice-assignment.sql
@@ -3,7 +3,10 @@
 
 -- 1. Select all movies with the 'Adventure' genre? Use the id.
 
-INSERT your SQL here :)
+SELECT * FROM "movies"
+JOIN "movies_genres" ON "movies".id = "movies_genres".movie_id
+JOIN "genres" ON "movies_genres".genre_id = "genres".id
+WHERE "genres".id = 1;
 
 -- 2. Get the count of movies that have each genre.  
 --  Make sure you get back all the genres!
@@ -21,21 +24,29 @@ Example Result:
 | Disaster      | 0             |
 ---------------------------------
 
-
+SELECT "genres".name, COUNT("movies") FROM "movies"
+JOIN "movies_genres" ON "movies".id = "movies_genres".movie_id
+JOIN "genres" ON "movies_genres".genre_id = "genres".id
+GROUP BY "genres".name;
 
 -- 3. Add the genre "Superhero" to "Star Wars".
 
-
+INSERT INTO "movies_genres" ("movie_id", "genre_id")
+VALUES (10, 13);
 
 -- 4. Remove the "Comedy" genre from "Titanic"
 
-
+DELETE FROM "movies_genres" WHERE "id" = 21;
 
 -- Stretch
 
 -- 1. How would you get all movies and all of their genres, but only one row per movie? (For example, on the list page we want to see all the movies and all of the movies' genres that apply)
 -- There're a few ways to do this, research ARRAY_AGG or JSON_AGG
 
+SELECT "movies".title, ARRAY_AGG("genres".name) AS "genres" FROM "movies"
+JOIN "movies_genres" ON "movies".id = "movies_genres".movie_id
+JOIN "genres" ON "movies_genres".genre_id = "genres".id
+GROUP BY "movies".title;
 
 -- 2. Delete the movie "The Martian". It has associated genres data...
 -- You may need to check out the ON DELETE CASCADE for the table columns...
